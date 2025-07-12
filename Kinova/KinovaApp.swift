9228227789890ee7b669b70b7c9793e157787e8b
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-07-12 23:04:13 +0200

    Initialize Kinova project with essential files (#1)
    - Add .gitignore to exclude unnecessary files
    - Create project.pbxproj for Xcode project configuration
    - Add workspace contents.xcworkspacedata for Xcode
    - Include AccentColor and AppIcon assets
    - Implement ContentView and KinovaApp Swift files
    - Set up Kinova.entitlements for app sandboxing

---
+++ Kinova/KinovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinovaApp.swift
+//  Kinova
+//
+//  Created by Igor Camilo on 12.07.25.
+//
+
+import SwiftUI
+
+@main
+struct KinovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
