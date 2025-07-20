b2d96a1f72ce556ac630097843a6898e302bd78a
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-07-20 16:08:14 +0200

    Add unit test target

---
+++ KinovaTests/KinovaTests.swift
@@ -0,0 +1,16 @@
+//
+//  KinovaTests.swift
+//  KinovaTests
+//
+//  Created by Igor Camilo on 20.07.25.
+//
+
+import Testing
+
+struct KinovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
